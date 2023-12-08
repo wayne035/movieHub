5a84bfd692986f552853de911d6a9c675b40603a
wayne <QWE24367705@gmail.com>
2023-12-08 14:15:06 +0800

    環境設置

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,14 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    extend: {},
+    colors:{
+      'main-color': '#ff5234',
+      'main-hover-color' : '#ef3515',
     },
   },
-  plugins: [],
+  plugins: [
+    require("tailwind-scrollbar-hide"),
+  ],
 }
 export default config
